9c67ad166f751d85d38f419f56d8f35cf87721c5
Rodrigo R. Galvao <rosattig@linux.vnet.ibm.com>
2018-07-24 09:27:14 -0300

    OOP refactoring and other small changes
    - Create a new class: Call
    - Small changes on Operator and Tlephony classes, regarding the way
    they deal with their variables (Python way?)
    - Other changes in the functions in order to accommodate with the previous
    changes
    - New function created: get_call()
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,29 +1,28 @@
 import cmd
 
-class Operator():
-    id = ''
-    # states: 0 - available | 1 - ringing | 2 - busy
-    state = 0
-
-    def __init__(self,id):
+class Call():
+    def __init__(self, id=""):
         self.id = id
 
-class CallOperator():
-    call_id = ''
-    op_id = ''
+class Operator():
 
-    def __init__(self, call_id, op_id):
-        self.call_id = call_id
-        self.op_id = op_id
+    # states: 0 - available | 1 - ringing | 2 - busy
+    def __init__(self,id="", state=0, current_call=""):
+        self.id = id
+        self.state = state
+        self.current_call = current_call
 
 class Telephony(cmd.Cmd):
-    calls = []
-    operators = [Operator("A"), Operator("B")]
-    call_operators = []
 
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = '(Command) '
+        self.operators = []
+        self.calls = []
+
+    def start(self):
+        self.operators.append(Operator("A"))
+        self.operators.append(Operator("B"))
 
     def do_call(self, call_id):
         # check if ID is valid
@@ -34,15 +33,16 @@ class Telephony(cmd.Cmd):
 
         print "Call ", call_id, " received"
 
+        call = Call(call_id)
         # add call ID to the calls list
-        self.calls.append(call_id)
+        self.calls.append(call)
         op = self.get_available_operator()
 
         # if there's an avialable operator, then assign this call to him
         if op:
             self.transfer_call_to_operator(op, call_id)
             # remove call form the calls list
-            self.calls.remove(call_id)
+            self.calls.remove(call)
         else:
             print "Call ", call_id, " waiting in queue"
 
@@ -51,38 +51,37 @@ class Telephony(cmd.Cmd):
         if not self.is_command_ok(op_id):
             return
 
-        for call_op in self.call_operators:
-            if call_op.op_id == op_id:
-                op = self.get_operator(op_id)
+        for op in self.operators:
+            if op.id == op_id:
                 # check if operator state == 1 (ringing). If so, then set it to 2 (busy)
                 if op.state == 1:
                     op.state = 2
-                    print "Call ", call_op.call_id, "answered by operator ", call_op.op_id
+                    call = self.get_call(op.current_call)
+                    print "Call ", op.current_call, "answered by operator ", op_id
                 else:
                     print "Operator ", op_id, " is busy at the moment."
-
 
     def do_reject(self, op_id):
         # check if ID is valid
         if not self.is_command_ok(op_id):
             return
 
-        for call_op in self.call_operators:
-            if call_op.op_id == op_id:
-                op = self.get_operator(op_id)
+        for op in self.operators:
+            if op.id == op_id:
                 # set operator state to 0 (available)
                 op.state = 0
-                print "Call ", call_op.call_id, " rejected by operator ", call_op.op_id
-                # remove this current call from call_operators list
-                self.call_operators.remove(call_op)
+                print "Call ", op.current_call, " rejected by operator ", op_id
                 # transfer the call to the next available operator
                 op2 = self.get_available_operator()
+                call = self.get_call(op.current_call)
+                current_call = op.current_call
+                op.current_call = ""
                 if op2:
-                    self.transfer_call_to_operator(op2, call_op.call_id)
+                    self.transfer_call_to_operator(op2, current_call)
                     # if the call is in the calls list, then just remove it
-                    if call_op.call_id in self.calls:
-                        self.calls.remove(call_op.call_id)
-
+                    call = self.get_call(current_call)
+                    if call:
+                        self.calls.remove(call)
                 return
 
     def do_hangup(self, call_id):
@@ -90,35 +89,35 @@ class Telephony(cmd.Cmd):
         if not self.is_command_ok(call_id):
             return
 
-        for call_op in self.call_operators:
-            if call_op.call_id == call_id:
-                op = self.get_operator(call_op.op_id)
+        call = self.get_call(call_id)
+
+        for op in self.operators:
+            if op.current_call == call_id:
                 # check if operator already answered the call (state == 2 [busy])
                 if op.state != 2:
                     print "Call ", call_id, " missed"
                 else:
-                    print "Call ", call_id, " finished and operator ", call_op.op_id, " available"
+                    print "Call ", call_id, " finished and operator ", op.id, " available"
 
                 # set operator state to 0 (available)
                 op.state = 0
+                op.current_call = ""
 
-                # remove this current call from call_operators list
-                self.call_operators.remove(call_op)
                 # check if there is a call waiting in the queue. If so, then transfer that call
                 # to an available operator
                 if self.calls:
                     # check operator availability
                     op2 = self.get_available_operator()
                     if op2:
-                        self.transfer_call_to_operator(op2, self.calls[0])
+                        self.transfer_call_to_operator(op2, self.calls[0].id)
                         # remove the call from the calls list
                         self.calls.remove(self.calls[0])
                 return
 
-        if call_id in self.calls:
-            # if call is not in the call_operators list, then it was just missed
+        if call:
+            # if call is not assigned to an operator yet, then it was just missed
             print "Call ", call_id, " missed"
-            self.calls.remove(call_id)
+            self.calls.remove(call)
 
     # used to exit 'cleanly'
     def do_exit(self, *args):
@@ -147,8 +146,8 @@ class Telephony(cmd.Cmd):
     def transfer_call_to_operator(self, op, call_id):
         # set operator state to 1 (ringing)
         op.state = 1
+        op.current_call = call_id
         print "Call ", call_id, " ringing at operator ", op.id
-        self.call_operators.append(CallOperator(call_id, op.id))
 
     # get the first available operator
     def get_available_operator(self):
@@ -161,9 +160,16 @@ class Telephony(cmd.Cmd):
         for op in self.operators:
             if op.id == id:
                 return op
-        print "Operator not found!"
+        return
+
+    # get the call corresponding to the given ID
+    def get_call(self, id):
+        for call in self.calls:
+            if call.id == id:
+                return call
         return
 
 if __name__ == '__main__':
     prompt = Telephony()
+    prompt.start()
     prompt.cmdloop()
